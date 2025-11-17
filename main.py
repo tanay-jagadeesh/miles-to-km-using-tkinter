7075c730ad3903184fd8b2b18f2dd176896e4025
Tanay <tanayj2107@gmail.com>
2025-11-17 15:54:41 -0500

    made the first label

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ window = Tk()
 window.title("Mile to Km Converter")
 
 window.minsize(height = 100, width = 300)
+
+# Making the labels
+
+my_label = Label("is equal to", font = ("Arial", 24, "bold"))
+my_label.grid(column = 1, row = 2)
