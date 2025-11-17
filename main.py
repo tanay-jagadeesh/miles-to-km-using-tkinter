83f7435a624c41a2a9519226fa5674f39ad9c4b5
Tanay <tanayj2107@gmail.com>
2025-11-17 17:07:57 -0500

    made second label

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ window.minsize(height = 100, width = 300)
 
 my_label = Label("is equal to", font = ("Arial", 24, "bold"))
 my_label.grid(column = 1, row = 2)
+
+my_label = Label("Miles", font = ("Arial", 24, "bold"))
+my_label.grid(column = 3, row = 1)
+
+
