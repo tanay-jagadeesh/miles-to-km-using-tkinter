961d7b828bc2c80f9b87088e296e14959a62892f
Tanay <tanayj2107@gmail.com>
2025-11-17 17:14:26 -0500

    added window.mainloop()

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ window.minsize(height = 100, width = 300)
 
 # Making the labels
 
-equal_label = Label(text = "is equal to", font = ("Arial", 24, "bold"))
+equal_label = Label(text = "is equal to", font = ("Arial", 15))
 equal_label.grid(column = 0, row = 1)
 
-miles_label = Label(text = "Miles", font = ("Arial", 24, "bold"))
+miles_label = Label(text = "Miles", font = ("Arial", 15))
 miles_label.grid(column = 2, row = 0)
 
-km_label = Label(text = "Km", font = ("Arial", 24, "bold"))
+km_label = Label(text = "Km", font = ("Arial", 15))
 km_label.grid(column = 2, row = 1)
 
 
+
+window.mainloop()
           
